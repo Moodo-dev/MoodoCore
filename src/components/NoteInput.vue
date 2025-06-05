6bd3fb0faa3b3f965a6718b52bd502623e773e46
Averyden <igo408gt@gmail.com>
2025-06-05 16:49:38 +0200

    Create base logic

---
+++ src/components/NoteInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <textarea
+      :value="modelValue"
+      placeholder="Add a note (optional)"
+      @input="$emit('update:modelValue', $event.target.value)"
+      rows="4"
+    />
+  </template>
+  
+  <script lang="ts" setup>
+    import { defineProps } from 'vue'
+    
+    const props = defineProps<{ modelValue: string }>()
+  </script>
+  
