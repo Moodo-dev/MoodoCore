d5527e9ff2847569fd94d2d6cc652548510b6f70
Averyden <igo408gt@gmail.com>
2025-06-05 16:41:22 +0200

    Set up base component for the selector

---
+++ src/components/MoodSelector.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="moodSelector">
+    </div>
+</template>
+
+<script lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{ modelValue: string | null}>()
+
+const moods = ['😃', '😐', '😞', '😡', '😴'] //Probably change later?
+
+</script>
