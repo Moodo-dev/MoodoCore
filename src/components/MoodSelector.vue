7719e770cb6d4de658a6375ab7fedf483eb593c1
Averyden <igo408gt@gmail.com>
2025-06-06 17:23:54 +0200

    stylize the selector

--- src/components/MoodSelector.vue
+++ src/components/MoodSelector.vue
@@ -19,3 +19,23 @@ const props = defineProps<{ modelValue: string | null }>()
 const moods = ['😃', '😐', '😞', '😡', '😴'] //Probably change later?
 // TODO: find out a way to make it a slider? 👀
 </script>
+
+<style scoped>
+.moodSelector button {
+  font-size: 2rem;
+  padding: 0.5rem 1rem;
+  background-color: #242424; /*TODO: find a way to not use a certain color*/
+  border: 2px solid transparent;
+  cursor: pointer;
+
+  transition:
+    background 0.3s,
+    border-color 0.3s ease;
+}
+
+.moodSelector button.selected {
+  border-color: #975294;
+  background-color: #8b2f87;
+  border-radius: 8px;
+}
+</style>
