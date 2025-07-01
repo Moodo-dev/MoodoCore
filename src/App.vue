939a132905283dcc59dad5f21f39efc9237f1f05
Averyden <igo408gt@gmail.com>
2025-07-01 17:26:53 +0200

    create a show notificiation method

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,16 @@ const notification = reactive({
   message: '',
   type: 'info' as 'info' | 'success' | 'error',
 })
+
+function showNotification(message: string, type: 'info' | 'success' | 'error' = 'info') {
+  notification.message = message
+  notification.type = type
+  notification.isVisible = true
+
+  setTimeout(() => {
+    notification.isVisible = false
+  }, 4000)
+}
 </script>
 
 <style>
