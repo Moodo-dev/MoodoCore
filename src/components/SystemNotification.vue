1c720a56ba8caea1c064bd5bb46257d5739a8e7a
Averyden <igo408gt@gmail.com>
2025-07-01 17:17:38 +0200

    define the basic structure of the component

--- src/components/SystemNotification.vue
+++ src/components/SystemNotification.vue
@@ -1,5 +1,21 @@
-<template></template>
+<template>
+  <Transition name="fade">
+    <div v-if="isVisible" :class="['notif', type]">
+      {{ message }}
+    </div>
+  </Transition>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { DefineProps, watch, ref } from 'vue'
+
+const props = defineProps<{
+  message: string
+  type?: 'success' | 'error' | 'info'
+  duration?: number
+}>()
+
+const isVisible = ref(true)
+</script>
 
 <style scoped></style>
