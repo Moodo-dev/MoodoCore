96ba13c9594dd2c99db4bdac4ba61ac4fe635b9b
Averyden <igo408gt@gmail.com>
2025-07-02 23:07:01 +0200

    Remove now redundant code, as our composable is what replaced the emit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h1>Moodo</h1>
     </header>
     <main>
-      <MoodLogger @notify="showNotification" />
+      <MoodLogger />
     </main>
 
     <SystemNotification />
@@ -12,25 +12,8 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
 import MoodLogger from './components/MoodLogger.vue'
 import SystemNotification from './components/SystemNotification.vue'
-
-const notification = reactive({
-  isVisible: false,
-  message: '',
-  type: 'info' as 'info' | 'success' | 'error',
-})
-
-function showNotification(message: string, type: 'info' | 'success' | 'error' = 'info') {
-  notification.message = message
-  notification.type = type
-  notification.isVisible = true
-
-  setTimeout(() => {
-    notification.isVisible = false
-  }, 4000)
-}
 </script>
 
 <style>
